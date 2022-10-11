925d051462fb5ebe8937d49634823817ed543fbb
nadillustrator <nadin2208@gmail.com>
2022-10-11 13:53:23 +0300

    HW2.11

---
+++ src/main/java/com/example/hw_sp2_11/Application.java
@@ -0,0 +1,13 @@
+package com.example.hw_sp2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
